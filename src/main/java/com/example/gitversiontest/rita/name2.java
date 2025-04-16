d2611c04721ed78d54516bc9e0911f5f471a5636
Rita Hung <hys910705@gmail.com>
2025-04-16 14:59:27 +0800

    kRita's first push

--- src/main/java/com/example/gitversiontest/rita/name2.java
+++ src/main/java/com/example/gitversiontest/rita/name2.java
@@ -1,4 +1,8 @@
 package com.example.gitversiontest.rita;
 
 public class name2 {
+    public static void main(String[] args){
+        System.out.println("Hello World Im Ritaaa");
+        System.out.println("Im the motherfkin train wreck");
+    }
 }
