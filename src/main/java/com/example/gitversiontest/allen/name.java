10209720715de1dcc943411c89725178998c5816
Allen <allen@gamil.com>
2025-04-16 16:02:56 +0800

    0416:修改name.java，加入hello allen訊息

--- src/main/java/com/example/gitversiontest/allen/name.java
+++ src/main/java/com/example/gitversiontest/allen/name.java
@@ -2,6 +2,7 @@ package com.example.gitversiontest.allen;
 
 public class name {
     public static void main(String[] args) {
-
+        System.out.println("Hello, Allen!");
+        System.out.println("This is a test for Git version control.");
     }
 }
