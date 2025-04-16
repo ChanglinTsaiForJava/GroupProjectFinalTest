cfcdb15ec237f2440e34313da07d32d3effd6d97
steve <changlin.stevetsai@gmail.com>
2025-04-16 10:47:47 +0800

    First commit

---
+++ src/main/java/com/example/gitversiontest/GitVersionTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitversiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitVersionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitVersionTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitversiontest/GitVersionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitversiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitVersionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
