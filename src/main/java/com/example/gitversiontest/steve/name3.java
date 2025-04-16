970574bdffa4f8c69004e7ca44f2837899038724
steve <changlin.stevetsai@gmail.com>
2025-04-16 11:08:00 +0800

    steve name

--- src/main/java/com/example/gitversiontest/steve/name3.java
+++ src/main/java/com/example/gitversiontest/steve/name3.java
@@ -3,5 +3,6 @@ package com.example.gitversiontest.steve;
 public class name3 {
     public static void main(String[] args) {
         System.out.println("Steve");
+        System.out.println("Hello Steve");
     }
 }
